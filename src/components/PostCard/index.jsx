ff24ac792b549ab46dbd45df1f782fc52083f35b
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-11-09 23:24:42 -0300

    :sparkles: feat(new): create a component to render post cards

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,17 @@
+import P from 'prop-types';
+
+export const PostCard = ({ cover, title, body }) => (
+  <div className="post">
+    <img src={cover} alt={title} />
+    <div className="post-content">
+      <h1>{title}</h1>
+      <p>{body}</p>
+    </div>
+  </div>
+);
+
+PostCard.propTypes = {
+  title: P.string.isRequired,
+  body: P.string.isRequired,
+  cover: P.string.isRequired,
+};
