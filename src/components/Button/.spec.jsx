616b3da72bf52a7cf28497bf62ffd7aa8215429b
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-11-16 00:29:12 -0300

    :white_check_mark: test(passing test): add new tests for button component

---
+++ src/components/Button/.spec.jsx
@@ -0,0 +1,43 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event';
+import { Button } from '.';
+
+describe('<Button />', () => {
+  it('should render the button with the text "Load more posts"', () => {
+    render(<Button text="Load more posts" disabled={ false } onClick={ () => {} } />);
+
+    // define number of assertions to make sure they are really happening
+    expect.assertions(2);
+
+    const button = screen.getByRole('button', { name: /Load more posts/i });
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveAttribute('class', 'button');
+  });
+
+  it('should call function on button click', () => {
+    const fn = jest.fn();
+    render(<Button text="Load more posts" disabled={ false } onClick={fn}/>);
+    expect.assertions(1);
+
+    const button = screen.getByRole('button', { name: /Load more posts/i });
+    fireEvent.click(button);
+    userEvent.click(button);
+    expect(fn).toHaveBeenCalledTimes(2);
+  });
+
+  it('should be disabled when disabled is true', () => {
+    render(<Button text="Load more posts" disabled={ true } onClick={ () => {} } />);
+    expect.assertions(1);
+
+    const button = screen.getByRole('button', { name: /Load more posts/i });
+    expect(button).toBeDisabled();
+  });
+
+  it('should not be disabled when disabled is false', () => {
+    render(<Button text="Load more posts" disabled={ false } onClick={ () => {} } />);
+    expect.assertions(1);
+
+    const button = screen.getByRole('button', { name: /Load more posts/i });
+    expect(button).toBeEnabled();
+  });
+});
