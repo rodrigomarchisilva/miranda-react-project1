d27adeac42d0b056581ef856f19edca3873419d5
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-11-17 23:28:26 -0300

    :clown_face: test(mock): add mock for componentDidMount on Home component test to deal with open handles

--- src/templates/Home/.spec.jsx
+++ src/templates/Home/.spec.jsx
@@ -3,6 +3,8 @@ import { Home } from '.';
 
 describe('<Home />', () => {
   it('renders the section with data-testid container', async () => {
+    const componentDidMount = jest.spyOn(Home.prototype, 'componentDidMount');
+    componentDidMount.mockImplementation(() => {});
     const { findByTestId } = render(<Home />);
     const sectionElement = await findByTestId('container');
     expect(sectionElement).toBeInTheDocument();
