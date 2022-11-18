4087fe36d6531b544754434616d172f629277f23
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-11-17 23:25:52 -0300

    :clown_face: test(mock): add mock for Posts test

---
+++ src/components/Posts/mock.js
@@ -0,0 +1,28 @@
+export const props = {
+  posts: [
+    {
+      id: 1,
+      title: 'title 1',
+      body: 'body 1',
+      cover: 'img/img1.png',
+    },
+    {
+      id: 2,
+      title: 'title 2',
+      body: 'body 2',
+      cover: 'img/img2.png',
+    },
+    {
+      id: 3,
+      title: 'title 3',
+      body: 'body 3',
+      cover: 'img/img3.png',
+    },
+    {
+      id: 4,
+      title: 'title 4',
+      body: 'body 4',
+      cover: 'img/img4.png',
+    },
+  ],
+};
