5de38787584746e431a07ea3ec886bf95919d96f
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-11-17 23:23:12 -0300

    :white_check_mark: test(passing test): add test suite with snapshot for PostCard

---
+++ src/components/PostCard/.spec.jsx
@@ -0,0 +1,19 @@
+import { screen, render } from "@testing-library/react";
+import { PostCard } from ".";
+import { props } from "./mock";
+
+describe('<PostCard />', () => {
+  it('should render PostCard correctly', () => {
+    expect.assertions(3);
+    render(<PostCard {...props} />);
+
+    expect(screen.getByRole('img', { name: /title 1/i })).toHaveAttribute('src', props.cover);
+    expect(screen.getByRole('heading', { name: /title 1/i })).toBeInTheDocument();
+    expect(screen.getByText('body 1')).toBeInTheDocument();
+  });
+
+  it('should match the snapshot', () => {
+    const { container } = render(<PostCard {...props} />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
