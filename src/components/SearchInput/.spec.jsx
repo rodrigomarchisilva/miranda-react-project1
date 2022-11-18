a784e0aa86e288fde127d08aa7e5d275a408389f
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-11-17 23:27:13 -0300

    :white_check_mark: test(passing test): edit test suite and add snapshot for SearchInput

--- src/components/SearchInput/.spec.jsx
+++ src/components/SearchInput/.spec.jsx
@@ -1,10 +1,25 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { SearchInput } from '.';
 
 describe('<SearchInput />', () => {
-  it('renders the placeholder', () => {
+  it('should render the SearchInput', () => {
     render(<SearchInput onChange={ () => {} } value="" />);
-    const inputElement = screen.getByPlaceholderText(/Type your search/i);
-    expect(inputElement).toBeInTheDocument();
+    const input = screen.getByPlaceholderText(/Type your search/i);
+    expect(input).toBeInTheDocument();
+    expect(input.value).toBe('');
   });
+
+  it('should call onChange as a key is pressed', () => {
+    const fn = jest.fn();
+    render(<SearchInput onChange={ fn } value="" />);
+    const input = screen.getByPlaceholderText(/Type your search/i);
+    userEvent.type(input, 'text');
+    expect(fn).toHaveBeenCalledTimes(4);
+  });
+
+  it('should match the snapshot', () => {
+    const { container } = render(<SearchInput onChange={ () => {} } value="" />);
+    expect(container.firstChild).toMatchSnapshot();
+  })
 });
