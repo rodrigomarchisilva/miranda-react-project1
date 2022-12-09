192ea2817d61b70456c6468534e30c2f4ae0c73b
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-12-08 23:24:13 -0300

    :clown_face: test(mock): add mocks for Home tests

---
+++ src/templates/Home/mock.js
@@ -0,0 +1,17 @@
+export const postsEndpoint = 'https://jsonplaceholder.typicode.com/posts';
+
+export const postsResponse = [
+  { userId: 1, id: 1, title: 'title 1', body: 'body 1' },
+  { userId: 2, id: 2, title: 'title 2', body: 'body 2' },
+  { userId: 3, id: 3, title: 'title 3', body: 'body 3' },
+];
+
+//----------------------------------------------------------------
+
+export const photosEndpoint = 'https://jsonplaceholder.typicode.com/photos';
+
+export const photosResponse = [
+  { url: 'img1.jpg' },
+  { url: 'img2.jpg' },
+  { url: 'img3.jpg' },
+];
