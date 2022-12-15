55226f133fb86c19d49659f3fd321565034458e5
rodrigomarchisilva <rodrigomarchisilva@gmail.com>
2022-12-15 16:11:12 -0300

    :clown_face: test(mock): include new values on mocks for Home template

--- src/templates/Home/mock.js
+++ src/templates/Home/mock.js
@@ -4,6 +4,7 @@ export const postsResponse = [
   { userId: 1, id: 1, title: 'title 1', body: 'body 1' },
   { userId: 2, id: 2, title: 'title 2', body: 'body 2' },
   { userId: 3, id: 3, title: 'title 3', body: 'body 3' },
+  { userId: 4, id: 4, title: 'title 4', body: 'body 4' },
 ];
 
 //----------------------------------------------------------------
@@ -14,4 +15,5 @@ export const photosResponse = [
   { url: 'img1.jpg' },
   { url: 'img2.jpg' },
   { url: 'img3.jpg' },
+  { url: 'img4.jpg' },
 ];
